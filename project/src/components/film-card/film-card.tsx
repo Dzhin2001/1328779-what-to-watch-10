19a5e7f688513295f2edae068bc6fd5503728058
Димитар <dzhin2001@mail.ru>
2022-08-04 12:55:01 +0300

    useEffect

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,6 +1,6 @@
 import {Film} from '../../types/films';
 import {Link} from 'react-router-dom';
-import {Dispatch} from 'react';
+import {Dispatch, useState, useRef, useEffect} from 'react';
 
 type FilmCardProps = {
   film: Film,
@@ -9,17 +9,49 @@ type FilmCardProps = {
 
 function FilmCard({film, setActiveCard}: FilmCardProps): JSX.Element {
   const id = film.id;
+  const [ needPlaying, setNeedPlaying] = useState(false);
+  const ref = useRef<HTMLVideoElement>(null);
+  const timerId = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    if (needPlaying) {
+      ref.current && ref.current.play();
+      return;
+    }
+    ref.current && ref.current.load();
+  }, [needPlaying]);
+
+  const mouseOverHandle = () => {
+    setActiveCard(id);
+    timerId.current = setTimeout(() => (setNeedPlaying(true)),1000);
+  };
+
+  const mouseOutHandle = () => {
+    setActiveCard(-1);
+    if (!needPlaying) {
+      timerId.current && clearTimeout(timerId.current);
+    }
+    setNeedPlaying(false);
+  };
+
   return (
     <article className="small-film-card catalog__films-card"
-      onMouseEnter ={(e) => {
-        setActiveCard(film.id);
-      }}
-      onMouseLeave ={(e) => {
-        setActiveCard(0);
-      }}
+      onMouseOver = {mouseOverHandle}
+      onMouseOut = {mouseOutHandle}
     >
       <div className="small-film-card__image">
-        <img src={film.posterImage} alt={film.name} width="280" height="175"/>
+        <video
+          ref={ref}
+          src={film.previewVideoLink}
+          poster={film.posterImage}
+          muted
+          width="280"
+          height="175"
+          controls={false}
+          loop={false}
+          autoPlay={false}
+        >
+        </video>
       </div>
       <h3 className="small-film-card__title">
         <Link to={`/films/${id.toString()}`} className="small-film-card__link">{film.name} </Link>
